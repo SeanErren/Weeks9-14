08a4b0c64a33ed79a8d081866b18dbdda0938d15
Sean Erren <ErrenSean@gmail.com>
2026-03-25 10:36:35 -0400

    Added better footstep soudns and randomized them on plalayer landing frames.

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Knight : MonoBehaviour
 {
     public AudioSource footsetpSound;
+    public List<AudioClip> audioClips;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +20,9 @@ public class Knight : MonoBehaviour
 
     public void FootStep()
     {
+        int randomClip = Random.Range(0, audioClips.Count - 1);
+        footsetpSound.clip = audioClips[randomClip];
+
         //SAVE PROJECT INSTEAD OF SAVE SCENE WILL MAKE SURE THAT THE ANIMATION LAYOUT WILL BE SAVED (should be saved anyway but just in case
         footsetpSound.Play();
     }
