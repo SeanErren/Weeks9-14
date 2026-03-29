0bc882c4ff87f7277fb72b7c60cf78a717fe7062
Sean Erren <ErrenSean@gmail.com>
2026-03-28 20:54:41 -0400

    Added particle system and a timer to the painters.

--- Assets/System project/Scripts/SprayColor.cs
+++ Assets/System project/Scripts/SprayColor.cs
@@ -1,8 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public class SprayColor : MonoBehaviour
 {
     public PointerMovement pointerScript; //For the position of the pointer
+    public ParticleSystem particles;
+    public TextMeshProUGUI timerText;
+
+    float cooldownTimer = 0, cooldown = 1.5f; //In seconds
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,15 +18,22 @@ public class SprayColor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+            timerText.text = cooldownTimer.ToString("F2"); //Showing digits after the dot because the cooldown is too short to only use seconds
+        }
+        else
+            timerText.text = "";
     }
 
     public void testEvent()
     {
-        //If the pointer is above the main painter (if its x value is the same)
-        if (transform.position.x == pointerScript.pointerPositions[pointerScript.currentPos])
+        //If the timer has reached the amount set and if the pointer is above the main painter (if its x value is the same)
+        if (cooldownTimer <= 0 && transform.position.x == pointerScript.pointerPositions[pointerScript.currentPos])
         {
-            gameObject.GetComponent<SpriteRenderer>().color = Random.ColorHSV();
+            particles.Play();
+            cooldownTimer = cooldown;
         }
     }
 }
