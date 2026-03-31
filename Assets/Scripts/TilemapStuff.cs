a7290c98293ea11565285a2aaa71cd8d36a0b148
Sean Erren <ErrenSean@gmail.com>
2026-03-31 12:01:31 -0400

    Cinemachine follow as well as the tilemaps through code and tilemaps drawing prefabs.

---
+++ Assets/Scripts/TilemapStuff.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+public class TilemapStuff : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Transform cheese;
+
+    public Tile flowers;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        //Vector3Int only accepts whole numbers because the tilemap doesn't have fractions
+        //Get the cell in the tilemap under the mouse pos
+        Vector3Int highlightedCell = tilemap.WorldToCell(mousePos);
+        //Get the center of the cell under the mouse
+        //tilemaps don't like Vector3
+        Vector3 cellCenter = tilemap.GetCellCenterWorld(highlightedCell);
+        Debug.Log(highlightedCell);
+        cheese.position = cellCenter;
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            tilemap.SetTile(highlightedCell, flowers);
+        }
+    }
+}
